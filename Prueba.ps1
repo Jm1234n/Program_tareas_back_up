f4cb310b276e3a4910b24c0bca328ec9b60c9f73
Joaquin Caceres Tapia <jcaceres@MTP.COM.PE>
2026-05-25 14:19:10 -0500

    Desarrollo de back up de todo tipo

---
+++ Prueba.ps1
@@ -0,0 +1,29 @@
+# Origen
+$origen = "D:\DESARROLLO\migracion de BD\BDP\exportacion de GDB\prueba"
+
+# Destino base
+$destinoBase = "D:\DESARROLLO\AUTOMATIZACION DE BACK UP\pRUEBA DE SALIDA"
+
+# Fecha
+$fecha = Get-Date -Format "yyyy-MM-dd_HH-mm"
+
+# Destino final
+$destino = Join-Path $destinoBase $fecha
+
+# Crear carpeta
+New-Item -ItemType Directory -Path $destino -Force
+
+# Log
+$log = Join-Path $destinoBase "backup_$fecha.log"
+
+# Copia
+robocopy `
+    $origen `
+    $destino `
+    /E `
+    /Z `
+    /R:3 `
+    /W:10 `
+    /MT:16 `
+    /TEE `
+    /LOG:$log
